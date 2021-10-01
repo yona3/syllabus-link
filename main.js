4b0d0cafc903e4b1043e9546ca877b229212cf69
yona3 <kou.y.0905@icloud.com>
2021-10-01 14:28:20 +0900

    add: generate syllabus url

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
+// generate syllabus url
 const url = window.location.href;
-console.log(url);
+const courseInfo = url.split("/")[5];
+const year = `20${courseInfo.slice(0, 2)}`;
+const courseId = courseInfo.slice(3);
+const syllabusURL = `https://tiglon.jim.u-ryukyu.ac.jp/portal/Public/Syllabus/DetailMain.aspx?lct_year=${year}&lct_cd=${courseId}`;
+console.log("syllabusURL: ", syllabusURL);
