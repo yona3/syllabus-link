07717658633a7e24ea3f76a0d64d883722c4a660
yona3 <kou.y.0905@icloud.com>
2021-10-01 14:36:55 +0900

    add: html element

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ const year = `20${courseInfo.slice(0, 2)}`;
 const courseId = courseInfo.slice(3);
 const syllabusURL = `https://tiglon.jim.u-ryukyu.ac.jp/portal/Public/Syllabus/DetailMain.aspx?lct_year=${year}&lct_cd=${courseId}`;
 console.log("syllabusURL: ", syllabusURL);
+
+// get target element
+const menu = document.getElementById("menu");
+const targetNav = menu.children[2];
+
+// create element
+const li = document.createElement("li");
+const a = document.createElement("a");
+a.textContent = "シラバス";
+a.href = syllabusURL;
+a.target = "_blank";
+
+li.appendChild(a);
+
+// add element
+targetNav.appendChild(li);
