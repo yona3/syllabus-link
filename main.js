23adbdc7b441e52e70cc77da7e23b754d6b5a6d2
yona3 <yonajs208@gmail.com>
2023-02-02 13:38:33 +0900

    refactor: main.js

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-// generate syllabus url
-const url = window.location.href;
-const courseInfo = url.split("/")[5];
-const year = `20${courseInfo.slice(0, 2)}`;
-const courseId = courseInfo.slice(3);
-const syllabusURL = `https://tiglon.jim.u-ryukyu.ac.jp/portal/Public/Syllabus/DetailMain.aspx?lct_year=${year}&lct_cd=${courseId}`;
+// 親要素
+const targetEl = (() => {
+  const menu = document.getElementById("menu");
+  return menu.children[2];
+})();
+
+// シラバスのURL生成
+const syllabusURL = (() => {
+  const url = window.location.href;
+  const courseInfo = url.split("/")[5];
+  const year = `20${courseInfo.slice(0, 2)}`;
+  const courseId = courseInfo.slice(3);
+  return `https://tiglon.jim.u-ryukyu.ac.jp/portal/Public/Syllabus/DetailMain.aspx?lct_year=${year}&lct_cd=${courseId}`;
+})();
 console.log("syllabusURL: ", syllabusURL);
 
-// get target element
-const menu = document.getElementById("menu");
-const targetNav = menu.children[2];
+// ボタンの生成
+const syllabusButton = (() => {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.textContent = "シラバス";
+  a.href = syllabusURL;
+  a.target = "_blank";
+  li.appendChild(a);
+  return li;
+})();
 
-// create element
-const li = document.createElement("li");
-const a = document.createElement("a");
-a.textContent = "シラバス";
-a.href = syllabusURL;
-a.target = "_blank";
-
-li.appendChild(a);
-
-// add element
-targetNav.appendChild(li);
+// ボタンの追加
+targetEl.appendChild(syllabusButton);
